ff569ccd2a483702dcffc6579f4eb1ce9c98ca00
Al-Amin <alamincse12@gmail.com>
2023-04-07 21:42:01 +0600

    added

--- testCases/conftest.py
+++ testCases/conftest.py
@@ -2,40 +2,45 @@ from selenium.webdriver.chrome.service import Service
 import pytest
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service as FirefoxService
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.firefox import GeckoDriverManager
 from selenium.webdriver.chrome.options import Options
-
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
 
 
 @pytest.fixture()
 def setup(browser):
 
     if browser == "chrome":
-        option = Options()
-        option.add_argument('--disable-notifications')
-        s = Service("E:\Soft\Python_PyCharm\chromedriver.exe")
-        driver = webdriver.Chrome(service=s, options=option)
-        driver.maximize_window()
         # option = Options()
         # option.add_argument('--disable-notifications')
-        # driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), chrome_options=option)
+        # s = Service("E:\Soft\Python_PyCharm\chromedriver.exe")
+        # driver = webdriver.Chrome(service=s, options=option)
+        # driver.maximize_window()
+
+        driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
         print("Launching Chrome Browser")
         return driver
+
     elif browser == 'firefox':
 
         # s = Service("E:\Soft\Python_PyCharm\geckodriver.exe")
         # driver = webdriver.Firefox(service=s)
         # driver.maximize_window()
+
         driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
         print("Launching Firefox Browser")
         return driver
 
+
     else:
 
-        s = Service("E:\Soft\Python_PyCharm\msedgedriver.exe")
-        driver = webdriver.Edge(service=s)
-        driver.maximize_window()
-        # driver = webdriver.Edge(EdgeChromiumDriverManager().install())
+        # s = Service("E:\Soft\Python_PyCharm\msedgedriver.exe")
+        # driver = webdriver.Edge(service=s)
+        # driver.maximize_window()
+
+        driver = webdriver.Edge(EdgeChromiumDriverManager().install())
         print("Launching Edge Browser")
         return driver
 
